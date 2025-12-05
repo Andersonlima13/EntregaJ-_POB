f889144a464f2c8259178d8c9df8611446e0f1a0
Júlio César <149001390+JulioCBMO@users.noreply.github.com>
2025-12-05 01:26:25 -0300

    revertendo

--- src/main/java/repositorio/EntregadorRepositorio.java
+++ src/main/java/repositorio/EntregadorRepositorio.java
@@ -11,28 +11,38 @@ public class EntregadorRepositorio extends CRUDRepositorio<Entregador> {
 
     @Override
     public Entregador ler(Object chave) {
-        if (chave instanceof Integer) {
-            int id = (Integer) chave;
-            Query q = manager.query();
-            q.constrain(Entregador.class);
-            q.descend("id").constrain(id);
-            List<Entregador> res = q.execute();
-            return res.isEmpty() ? null : res.get(0);
-        } else if (chave instanceof String) {
-            String nome = (String) chave;
-            Query q = manager.query();
-            q.constrain(Entregador.class);
-            q.descend("nome").constrain(nome);
-            List<Entregador> res = q.execute();
-            return res.isEmpty() ? null : res.get(0);
+        conectar();
+        try {
+            if (chave instanceof Integer) {
+                int id = (Integer) chave;
+                Query q = manager.query();
+                q.constrain(Entregador.class);
+                q.descend("id").constrain(id);
+                List<Entregador> res = q.execute();
+                return res.isEmpty() ? null : res.get(0);
+            } else if (chave instanceof String) {
+                String nome = (String) chave;
+                Query q = manager.query();
+                q.constrain(Entregador.class);
+                q.descend("nome").constrain(nome);
+                List<Entregador> res = q.execute();
+                return res.isEmpty() ? null : res.get(0);
+            }
+            return null;
+        } finally {
+            desconectar();
         }
-        return null;
     }
 
     public List<Entregador> listarPorNome(String texto) {
-        Query q = manager.query();
-        q.constrain(Entregador.class);
-        q.descend("nome").constrain(texto);
-        return q.execute();
+        conectar();
+        try {
+            Query q = manager.query();
+            q.constrain(Entregador.class);
+            q.descend("nome").constrain(texto);
+            return q.execute();
+        } finally {
+            desconectar();
+        }
     }
 }
